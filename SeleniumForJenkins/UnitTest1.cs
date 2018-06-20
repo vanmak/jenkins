93ba11735a0db1c61d8f6906309545c99c06a3fa
Ivan Makogon <makivan@e-mail.ua>
2018-06-21 00:01:37 +0300

    jen test

---
+++ SeleniumForJenkins/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumForJenkins
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var driver = new ChromeDriver();
+            driver.Url = "http://www.google.com";
+            Assert.AreEqual(driver.Title, "Google");
+
+        }
+    }
+}
